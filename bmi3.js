20ac49ba12f0b4448b4b66e64ff760bea24e838b
fatima zahra <fma87879@gmail.com>
2024-04-27 00:10:09 +0100

    Update bmi3.js

--- bmi3.js
+++ bmi3.js
@@ -2,36 +2,38 @@ $("button").on("click",function(){
     var weight=$("#n1").val();
     var height=$("#n2").val();
     var bmi=weight/((height/100)**2);
+    var result=$("#result");
+    var img=$("#img");
     if (bmi < 16){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img1.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img1.jpeg");
     }
     else if(bmi >= 16 && bmi < 17){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img2.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img2.jpeg");
     }
     else if(bmi >= 17 && bmi < 18.5){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img3.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img3.jpeg");
     }
     else if(bmi >= 18.5 && bmi < 25){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img4.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img4.jpeg");
     }
     else if(bmi >= 25 && bmi < 30){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img5.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img5.jpeg");
     }
     else if(bmi >= 30 && bmi < 35){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img6.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img6.jpeg");
     }
     else if(bmi >= 35 && bmi < 40){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img7.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img7.jpeg");
     }
     else if(bmi >= 40){
-        $("#result").text("your BMI:"+bmi);
-        $("#img").attr("src","img8.jpeg");
+        result.text("your BMI:"+bmi);
+        img.attr("src","img8.jpeg");
     }
 })
